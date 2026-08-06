077f48eb3a48457fb65720ccfda140a6e3fce1c1
ThanhDNguyen7696 <134131586+ThanhDNguyen7696@users.noreply.github.com>
2026-08-06 10:28:22 +1000

    Add age verification functions

--- JS/age-verification.js
+++ JS/age-verification.js
@@ -1,1 +1,14 @@
+const storageKey = "winebankAgeVerified";
+
+export function hasVerifiedAge() {
+  return localStorage.getItem(storageKey) === "true";
+}
+
+export function verifyAge() {
+  localStorage.setItem(storageKey, "true");
+}
+
+export function clearAgeVerification() {
+  localStorage.removeItem(storageKey);
+}
 
